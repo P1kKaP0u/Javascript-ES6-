6b5aab8c84f4efcb1a380b4aa7911d91cad09ee2
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2022-11-01 20:54:53 +0300

    dom manipulation node'lar ile çalışma

--- Dom_Manipulation/index.js
+++ Dom_Manipulation/index.js
@@ -30,12 +30,28 @@
 // alert(nameElements[0].value);
 
 
-var name = document.getElementById("name").addEventListener("click",changeColor);
+// var name = document.getElementById("name").addEventListener("click",changeColor);
 
-function changeColor(){
-    document.getElementById("div1").style.backgroundColor = "red";
-    document.getElementById("div1").style.color = "white";
-    document.getElementById("div1").style.fontSize = "35px";
-    var nameElements = document.getElementsByName("userName");
-    nameElements[0].value = "Mustafa Kemal"
-}
+// function changeColor(){
+//     document.getElementById("div1").style.backgroundColor = "red";
+//     document.getElementById("div1").style.color = "white";
+//     document.getElementById("div1").style.fontSize = "35px";
+//     var nameElements = document.getElementsByName("userName");
+//     nameElements[0].value = "Mustafa Kemal"
+// }
+
+
+// var node = document.getElementById("tree");
+// alert(node.childNodes[0].nodeValue);
+
+
+var head = document.createElement("h2");
+var node = document.createTextNode("Merhaba, JavaScript");
+head.appendChild(node);
+
+var div1 = document.getElementById("div1");
+var p2 = document.getElementById("p2");
+div1.insertBefore(head, p2);
+
+alert("P2 Kaldırılıyor!!!!!!!!");
+div1.removeChild(p2);
